31432049e05c874bc46219c984878c4b0b6507b0
Kirill Pavlovskiy <kirill.s.pavlovskiy@gmail.com>
2025-09-28 12:59:21 -0500

    Help code done

--- src/Talk.zig
+++ src/Talk.zig
@@ -10,8 +10,13 @@ pub fn main() !void {
     std.debug.print("Alocator done \n", .{});
 
     var argumentItterator = try std.process.argsWithAllocator(allocator);
+    defer argumentItterator.deinit();
 
     var i: usize = 1;
     i = 1;
-    while (argumentItterator.next()) |arg| : (i += 1) std.debug.print("arg# {}: {s}\n", .{ i, arg });
+    while (argumentItterator.next()) |arg| : (i += 1) {
+        if (std.mem.eql(u8, arg, "-help") | std.mem.eql(u8, arg, "-h")) {
+            std.debug.print("help menue triggered, the program will self destruct in 10 seconds", .{});
+        }
+    }
 }
